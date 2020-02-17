5092ba912d5155abfa8ac151c22207bdaabe22e9
kowasaki17 <herrmann@gebrauchsgrafiker.de>
2020-02-17 15:14:22 +0100

    Solved with help

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,5 +3,22 @@ import '../styles/main.scss';
 // Import any additional modules you want to include below \/
 
 
-// \/ All of your javascript should go here \/
+// \/ All of your javascript should go here 
+const modal = document.getElementById("myModal");
 
+let promiseOfModal = new Promise ((resolve, reject)=>{
+    setTimeout(() => {
+        resolve(modal)
+    }, 2000);
+})
+promiseOfModal.then(function(modal){
+    modal.style.display = "block";
+})
+modal.addEventListener("click", (e) => {
+    switch(e.target.className) {
+        case "close":
+        case "modal":
+        modal.style.display = "none"
+        break;
+    }
+})
